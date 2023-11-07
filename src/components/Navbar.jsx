4c7987693ad6133825a68c16d73678e33ced0244
TAnDe07 <tdesir1@ufl.edu>
2023-11-07 12:39:06 -0500

    pdf changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -90,7 +90,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#ED2224]'>
             <a
               className='flex justify-between items-center w-full text-gray-400'
-              href = "my-website/src/assets/Tana_Desir_Resume.pdf"
+              href = "../assets/Tana_Desir_Resume.pdf"
               target='_blank'
             >
               Resume <GrDocumentPdf size={25} />
