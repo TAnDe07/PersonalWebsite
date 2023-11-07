d9703ff259fd97017d698969eb64bbf06b3975d0
TAnDe07 <tdesir1@ufl.edu>
2023-11-07 14:40:35 -0500

    pdf and color change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,19 +8,19 @@ const Home = () => {
   return (
     <div
     name="home"
-    className="h-screen w-full bg-[#0b2736]"
+    className="h-screen w-full bg-[#E5EDF1]"
   >
     <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
       <div className="flex flex-col justify-center h-full">
-        <h2 className="text-4xl sm:text-7xl font-bold text-white">
-          WELCOME
+        <h2 className="text-4xl sm:text-7xl font-bold" style={{color: '#96C2DB', backgroundColor: '#E5EDF1'}}>
+          Welcome
         </h2>
-        <p className="text-gray-400 py-4 max-w-md">
-       Hi there! I am a third-year Computer Science student at the University of Florida. I am interested in software engineering, game development, and 
+        <p className="text-gray-400 py-4 max-w-md" style={{color: '#808080', backgroundColor: '#E5EDF1'}}>
+       Hi there! My name is Tana Desir, and I am a third-year Computer Science student at the University of Florida. I am interested in software engineering, game development, and 
         work related to Human-Centered Computing. Ultimately, I want to use computer science to address 
-        real world societal problems.
+        real world societal problems. Feel free to connect with me on LinkedIn!
         <div></div>
-        PS : I work on this website on my free time, therefore it's still being updated :)
+        PS : I work on this website on my free time, so it's still being updated :)
         </p>
         {/*<div>
           <Link
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,13 +13,13 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[70px] flex justify-between items-center px-5 bg-[#0b2736] text-gray-300'>
+    <div className='fixed w-full h-[63px] flex justify-between items-center px-5 bg-[#dbaf96] text-gray-300'>
       <div>
         {/*add logo later*/}
-        <h1 className=' font-thin text-2xl italic font-serif'>Tana Desir</h1>
+        <h1 className=' font-thin text-2xl italic font-serif'style={{color: '#808080', textShadow: '4px 4px 2px #96c2db'}}>TD</h1>
       </div>
       {/* menu */}
-      <ul className='hidden md:flex gap-x-8'>
+      <ul className='hidden md:flex gap-x-8' style={{ color: '#808080' }}>
         <li>
           <Link to='Home' smooth={true} duration={500}>
             Home
@@ -37,7 +37,7 @@ const Navbar = () => {
   </li>*/}
       </ul>
       {/* Hamburger */}
-      <div onClick={handleClick} className='md:hidden z-10'>
+      <div onClick={handleClick} className='md:hidden z-10'  style={{ color: '#808080' }}>
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
       {/* Mobile menu */}
@@ -45,33 +45,33 @@ const Navbar = () => {
         className={
           !nav
             ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-[#0b2736] flex flex-col justify-center items-center'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#E5EDF1] flex flex-col justify-center items-center'
         }
       >
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-4xl' style={{ color: '#808080' }}>
           <Link onClick={handleClick} to='home' smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        {/*<li className='py-6 text-4xl' style={{ color: '#808080' }}>
           {' '}
           <Link onClick={handleClick} to='about' smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-4xl' style={{ color: '#808080' }}>
           {' '}
           <Link onClick={handleClick} to='project' smooth={true} duration={500}>
             Project
           </Link>
-        </li>
+      </li>*/}
       </ul>
       {/* Social icons */}
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0E76A8]'>
             <a
-              className='flex justify-between items-center w-full text-gray-400'
+              className='flex justify-between items-center w-full text-white'
               href='https://www.linkedin.com/in/tana-desir'
               target='_blank'
               >
@@ -80,7 +80,7 @@ const Navbar = () => {
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
-              className='flex justify-between items-center w-full text-gray-400'
+              className='flex justify-between items-center w-full text-gray-300'
               href='https://github.com/TAnDe07'
               target='_blank'
             >
@@ -89,8 +89,8 @@ const Navbar = () => {
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#ED2224]'>
             <a
-              className='flex justify-between items-center w-full text-gray-400'
-              href="https://drive.google.com/file/d/10b5GJxsR6ldRHDESzcR20lQh87am2SNc/view?usp=sharing"
+              className='flex justify-between items-center w-full text-white'
+              href="https://drive.google.com/file/d/1yKBQ8DS29XYrNwcl81HcrUt0UtDMjT7y/view?usp=sharing"
               target='_blank'
             >
               Resume <GrDocumentPdf size={25} />
